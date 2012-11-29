1ec34185c8324a2ed17c42ac7a0ce921a3b7f1b8
Jared Barnett <jared@jaredbarnett.com>
2012-11-28 21:48:35 -0700

    slide sprite

--- js/slides.js
+++ js/slides.js
@@ -1,6 +1,7 @@
 var Slide = function($el, config) {
 	this.$el = $el;
 	this.config = config;
+	this.numSlides = config.slides && config.slides.length || 0;
 
 	this.width = $el.width();
 	this.height = $el.height();
@@ -18,7 +19,7 @@ Slide.prototype.init = function() {
 	if (slides.length) {
 		this.addControls();
 		this.goTo(0);
-		if (config.autostart) {
+		if (config.autostart) { 
 			this.play();
 		}
 		this.load(1); 
@@ -29,7 +30,7 @@ Slide.prototype.play = function() {
 	var self = this;
 	this.interval = setInterval(function(){
 		self.advance();
-		self.load(self.i + 1); //preload the next one
+		self.preload(); 
 	}, this.config.speed);
 	this.isPlaying = true;
 };
@@ -47,23 +48,37 @@ Slide.prototype.resetInterval = function(){
 };
 
 Slide.prototype.advance = function() {
-	this.i = this.i + 1;
-	this.goTo(this.i);
+	var next = (this.i + 1) % this.numSlides;
+	
+	this.i = next;
+	this.goTo(next);
 	this.resetInterval();
 };
 
 Slide.prototype.recede = function() {
-	this.i = this.i - 1;
-	this.goTo(this.i);
+	var i = this.i,
+		next = (i === 0 ? this.numSlides : i) - 1;
+
+	this.i = next;
+	this.goTo(next);
 	this.resetInterval();
 };
 
 Slide.prototype.addControls = function(){
 	var a = [];
 
-	a.push('<div class="icon pause"></div>');
+	a.push('<div class="controls">');
+
+	if (this.config.autostart) {
+		a.push('<div class="icon pause"></div>');
+	}
+	else {
+		a.push('<div class="icon play"></div>');
+	}
 	a.push('<div class="icon prev"></div>');
 	a.push('<div class="icon next"></div>');
+
+	a.push('</div>');
 
 	this.$el.append(a.join(''));
 	this.bindControlEvents();
@@ -74,9 +89,13 @@ Slide.prototype.bindControlEvents = function(){
 		self = this;
 
 	$el.mouseenter(function(){
-		$(this).find('.icon').fadeTo(200, 0.7);
+		$(this).find('.controls').stop().animate({
+			bottom: "0px"
+		}, 300);
 	}).mouseleave(function(){
-		$(this).find('.icon').fadeTo(200, 0);
+		$(this).find('.controls').stop().animate({
+			bottom: "-55px"
+		}, 500);
 	});
 
 	$el.on('click', '.icon', function(e){
@@ -148,18 +167,31 @@ Slide.prototype.goTo = function(i) {
 	}
 };
 
+Slide.prototype.preload = function() {
+	var numSlides = this.config.slides.length || 0,
+		nextIndex = (this.i + 1) % numSlides;
+
+	this.load(nextIndex);
+};
+
 Slide.prototype.load = function(i, callback) {
 	var self = this,
 		slides = this.config.slides,
 		slide,
 		html,
-		src;
+		src,
+		id = "s" + (+new Date);
 
 	if (i < 0 || i >= slides.length) {
 		return;
 	}
 
 	slide = slides[i];
+
+	if (slide.loading || slide.$el !== undefined) {
+		return;
+	}
+
 	slide.loading = true;
 	src = slide.src;
 
@@ -167,10 +199,10 @@ Slide.prototype.load = function(i, callback) {
 		return;
 	}
 
-	html = '<div id="latest-slide" class="slide offpage"><img src="'+src+'"></div>';
+	html = '<div id="'+id+'" class="slide offpage"><img src="'+src+'"></div>';
 	this.$el.append(html);
 
-	$('#latest-slide img').load(function(){
+	$('#'+id+' img').load(function(){
 		var $img = $(this),
 			$slide = $img.closest('.slide'),
 			w = self.width,
@@ -202,7 +234,7 @@ Slide.prototype.load = function(i, callback) {
 		$slide.removeAttr('id').removeClass('offpage').addClass('hide');
 
 		slide.$el = $slide;
-		if (callback !== undefined) {
+		if (callback !== undefined) { 
 			callback($slide);
 		}
 	});
@@ -223,6 +255,7 @@ $.fn.slide = function(config) {
 
 $(document).ready(function(){
 	$('#home-slides').slide({
+		autostart: false,
 		slides: (function(){
 			var count = 93,
 				i = 0,
@@ -235,4 +268,13 @@ $(document).ready(function(){
 			return a;
 		})()
 	});
+
+	$('#picnic-slides').slide({
+		autostart: false,
+		slides: [
+			{src: "img/ebengfine_0.jpg"},
+			{src: "img/ebengfine_ropeswing.jpg"},
+			{src: "img/ebengfine_rocks.jpg"}
+		]
+	})
 });
