103b405ba22e17f4d5a1b6aea54aa04e0cf5e410
Jared Barnett <jared@jaredbarnett.com>
2012-12-11 17:45:42 -0700

    html5shiv ftw

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 var Countdown = function(){
 	var $cd = $('#countdown-days'),
-		$container = $cd.closest('.callout'),
+		$container = $cd.closest('.countdown'),
 		daysleft = Math.floor((new Date(2013, 5, 22) - new Date) / (1000*60*60*24));
 
 	setTimeout(function(){
-		$cd.hide().text(daysleft).fadeIn(500);
+		$cd.text(daysleft);
+		$container.hide().removeClass('hide').fadeIn(500);
 	}, 200);
 };
 
